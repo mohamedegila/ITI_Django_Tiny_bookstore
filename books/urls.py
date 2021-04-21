518a3ed72ca8189e06f7204d71da2c56efe21fd1
mohammedegila <mohammedegila@gmail.com>
2021-04-21 15:28:44 +0200

    add create and edit

--- books/urls.py
+++ books/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index,name="index"),
+    path('create', views.create,name="create"),
+    path('edit/<int:id>', views.edit,name="edit")
 ]
--- books/views.py
+++ books/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .forms import BookForm
 from django.http import HttpResponse
+from .models import Book
 # Create your views here.
 
 def index(request):
-    return render (request,"books/index.html")
+    books = Book.objects.all()
+    return render (request,"books/index.html",{
+        "books" : books
+    })
+
+
+def create(request):
+    form = BookForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect("index")
+    return render(request,"books/create.html",{
+        "form" : form
+    })
+
+def edit(request,id):
+
+    book = Book.objects.get(pk=id)
+    form = BookForm(request.POST or None , instance=book)
+    if form.is_valid():
+        form.save()
+        return redirect("index")
+    return render(request,"books/edit.html",{
+        "form" : form,
+        "book" : book
+    })
