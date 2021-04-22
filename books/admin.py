2a3da62d836107eadc9d365408f4551acf22555e
mohammedegila <mohammedegila@gmail.com>
2021-04-22 16:56:07 +0200

    Add category validation

--- books/admin.py
+++ books/admin.py
@@ -1,17 +1,20 @@
 from django.contrib import admin
 from .models import Book, Category, Isbn, Tag
 
-from .forms import BookForm
+from .forms import BookForm, CategoryForm
 from django.contrib.auth.models import User
 
 # Register your models here.
 
 class BookAdmin(admin.ModelAdmin):
-
     form = BookForm
     list_display  = ('title','author')
+    ordering = ('title',)
     list_filter   = ('categories',)
     search_fields = ('title',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    form = CategoryForm
 
 class BookInline(admin.StackedInline):
     model    = Book
@@ -20,10 +23,13 @@ class BookInline(admin.StackedInline):
 
 class TagAdmin(admin.ModelAdmin):
     inlines = [BookInline]
+
+class IsbnAdmin(admin.ModelAdmin):
+    inlines = [BookInline]
     
 admin.site.register(Book,BookAdmin)
-admin.site.register(Category)
-admin.site.register(Isbn)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Isbn,IsbnAdmin)
 admin.site.register(Tag,TagAdmin)
 
 
--- books/forms.py
+++ books/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Book
+from .models import Book, Category
 from django.core.exceptions import ValidationError 
 
 class BookForm(forms.ModelForm):
@@ -11,10 +11,23 @@ class BookForm(forms.ModelForm):
     def clean(self):
         super(BookForm,self).clean()
         title    = self.cleaned_data.get('title')
-        category = self.cleaned_data.get('categories')
-
+    
         if len(title) < 10 or len(title) > 50:
             raise ValidationError("Title must be in between 10 & 50")
-        if len(category) < 2:
-            raise ValidationError("Category must be at least 2")
+       
+        return self.cleaned_data
+
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model   = Category
+        fields  = "__all__"
+
+    def clean(self):
+        super(CategoryForm,self).clean()
+
+        category_name = self.cleaned_data.get('name')
+
+        if len(category_name) < 2:
+            raise ValidationError("Category name must be at least 2")
         return self.cleaned_data
