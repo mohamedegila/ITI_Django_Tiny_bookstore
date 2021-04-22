d10b0f3740067caf9d66c4125c4b4510c69cc7b5
mohammedegila <mohammedegila@gmail.com>
2021-04-22 15:13:11 +0200

    Add iAdd form validation

--- books/admin.py
+++ books/admin.py
@@ -4,6 +4,9 @@ from .models import Category
 from .models import Isbn
 # Register your models here.
 
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title','author')
+
 admin.site.register(Book)
 admin.site.register(Category)
 admin.site.register(Isbn)
--- books/forms.py
+++ books/forms.py
@@ -1,8 +1,20 @@
 from django import forms
 from .models import Book
+from django.core.exceptions import ValidationError 
 
 class BookForm(forms.ModelForm):
     class Meta:
         model   = Book
         fields  = "__all__"
         exclude = ("isbn",)
+
+    def clean(self):
+        super(BookForm,self).clean()
+        title    = self.cleaned_data.get('title')
+        category = self.cleaned_data.get('categories')
+
+        if len(title) < 10 or len(title) > 50:
+            raise ValidationError("Title must be in between 10 & 50")
+        if len(category) < 2:
+            raise ValidationError("Category must be at least 2")
+        return self.cleaned_data
