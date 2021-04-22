3472c121011b48211b0e12a45e5ba007c732537c
mohammedegila <mohammedegila@gmail.com>
2021-04-22 15:24:53 +0200

    Add validation to admin

--- books/admin.py
+++ books/admin.py
@@ -2,12 +2,17 @@ from django.contrib import admin
 from .models import Book
 from .models import Category
 from .models import Isbn
+from .forms import BookForm
 # Register your models here.
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title','author')
 
-admin.site.register(Book)
+    form = BookForm
+    list_display  = ('title','author')
+    list_filter   = ('categories',)
+    search_fields = ('title',)
+
+admin.site.register(Book,BookAdmin)
 admin.site.register(Category)
 admin.site.register(Isbn)
 
