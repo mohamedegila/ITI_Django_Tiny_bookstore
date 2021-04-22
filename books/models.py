033fdc9eed54ecac33a4350916aa9980bf519cff
mohammedegila <mohammedegila@gmail.com>
2021-04-22 13:47:12 +0200

    Add Isbn model and relationships

--- books/models.py
+++ books/models.py
@@ -1,16 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Category(models.Model):
+    class Meta:
+        pass
+
     name       = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True) 
+
+
+    def __str__(self):
+        return self.name
+
+class Isbn(models.Model):
+    author_title = models.CharField(max_length=255,null=True,blank=True)
+    book_title   = models.CharField(max_length=255,null=True,blank=True)
+    isbn_number  = models.IntegerField(max_length=50,auto_created=True,null=True,blank=True)
+
+
+    def __str__(self):
+        return self.book_title
 
 class Book(models.Model):
     title       = models.CharField(max_length=255)
     description = models.TextField(max_length=2048)
+    categories  = models.ManyToManyField(Category) 
+    author      = models.ForeignKey(User,null=True,on_delete=models.CASCADE, related_name="book")
 
-
+    isbn        = models.OneToOneField(Isbn,on_delete=models.CASCADE,null=True,blank=True)
+    
     def __str__(self):
         return self.title
  
