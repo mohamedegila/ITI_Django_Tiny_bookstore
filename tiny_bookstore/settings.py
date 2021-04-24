8abdecb6efd750ec0e3cb0a7a3598a878333405a
mohammedegila <mohammedegila@gmail.com>
2021-04-24 13:45:20 +0200

    add rest framework

--- tiny_bookstore/settings.py
+++ tiny_bookstore/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'books.apps.BooksConfig',
     'accounts',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
